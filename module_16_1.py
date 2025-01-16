ecee333a3c8b9e6cea4794d5c7fcf20ec9a58d3f
MarinaHarbour <shunatovamarina@gmail.com>
2025-01-16 15:03:31 +0200

    Add files via upload

--- module_16_1.py
+++ module_16_1.py
@@ -6,18 +6,18 @@ app = FastAPI()
 
 @app.get("/")
 async def home_page():
-    return {"message": "Главная страница"}
+    return "Главная страница"
 
 
 @app.get("/user/admin")
 async def page_admin():
-    return {"message": "Вы вошли как администратор"}
+    return "Вы вошли как администратор"
 
 
 @app.get("/user/{user_id}")
-async def id_user(user_id: int) -> dict:
-    return {"message": f"Вы вошли как пользователь № {user_id}"}
+async def id_user(user_id: int) -> str:
+    return f"Вы вошли как пользователь № {user_id}"
 
 @app.get("/user")
-async def info(username: str, age: int) -> dict:
-    return {"Информация о пользователе. Имя:": {username}, "Возраст:": {age}}
+async def info(username: str, age: int) -> str:
+    return f"Информация о пользователе. Имя: {username}, Возраст: {age}"
